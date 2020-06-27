9e1175799d4cd1d3a18c58834e7f964b793d4641
Leela9528 <66050414+Leela9528@users.noreply.github.com>
2020-06-27 14:48:42 +0530

    script.js-changed

--- script.js
+++ script.js
@@ -59,9 +59,9 @@ WARNING!!! WARNING!!!
       // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
       // name in the loop.
       if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
+        byeSpeaker(names[i]);
       } else {
-        helloSpeaker.speak(names[i]);
+        helloSpeaker(names[i]);
       }
     }
     
